4ef43e22907de9a54a2ed69c6859ed0619a2efa9
john <john@jpatmore.plus.com>
2020-11-29 02:27:33 +0000

    Modified primes calculator. Original at <https://www.geeksforgeeks.org/sieve-of-eratosthenes/>

---
+++ main.py
@@ -0,0 +1,29 @@
+# https://www.geeksforgeeks.org/sieve-of-eratosthenes/
+
+# Python program to print all primes smaller than or equal to
+# n using Sieve of Eratosthenes
+
+def sieve_of_eratosthenes(n):
+    # Create a boolean array "prime[0..n]" and initialize
+    # all entries it as true. A value in prime[i] will
+    # finally be false if i is NOT a prime, else true.
+    prime = [True for _ in range(n + 1)]
+    p = 2
+    while p * p <= n:  # p <= sqrt(n) but sqrt() is comparatively slow!
+
+        # If prime[p] is not changed, then it is a prime
+        if prime[p]:
+            # Update all multiples of p
+            for i in range(p * p, n + 1, p):
+                prime[i] = False
+        p += 1
+
+    # Print all prime numbers
+    for p in range(2, n + 1):
+        if prime[p]:
+            print(f"{p}, ")
+
+    # driver program
+    if __name__ == '__main__':
+        n = 30
+        print(f"Following are the prime numbers smaller than or equal to {n} {sieve_of_eratosthenes(n)}")
